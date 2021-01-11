c09bf2512958bf8721cf6895f1aefdd8ddb21565
Vadimmmmm <alexnik1970@tut.by>
2021-01-11 23:33:30 +0300

    add_funck_rec

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from multiprocessing import Process
 word= ''
 
 def find_all_links(url: str) -> list:
-    text_on_page = requests.get(url).text
+    try:
+        text_on_page = requests.get(url).text
+    except:
+        print('Некорректный ')
     if text_on_page.find(word) == -1:
         soup = BeautifulSoup(text_on_page, 'html.parser')
         all_links = [i.get('href') for i in soup.find_all('a')]
@@ -13,11 +16,27 @@ def find_all_links(url: str) -> list:
     else:
         return url
 
+def find_word_in_links_on_pages(url:str, count=0):
+    r = find_all_links(url)
+    if count == 2:
+        print('end')
+        return
+    if type(r) == str:
+        return r
+    else:
+        for i in r:
+            print(i)
+            find_word_in_links_on_pages(i,count + 1)
+
 def _main():
     url = 'https://www.youtube.com/'
     links = find_all_links(url)
+    processes = []
     for link in links:
-        pass
+        processes.append(Process(target=find_word_in_links_on_pages, args=(link)))
+    for process in processes:
+        process.run()
+        process.join()
 
 
 if __name__ == '__main__':
